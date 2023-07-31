7ed18a936b92116a849e1e074d1c3d687bcf8d47
Siddhant Gunjal <87136071+Siddhant1401@users.noreply.github.com>
2023-07-31 11:39:37 +0530

    Update User.js

--- 08-Tic-Tac-Toe-Facade-Design-Pattern/User.js
+++ 08-Tic-Tac-Toe-Facade-Design-Pattern/User.js
@@ -1,12 +1,11 @@
 const Game = require("./Game");
 
 let g1 = Game.newGame("Sid","Ram")
-let g2 = Game.newGame("Ramesh","Suresh")
+//let g2 = Game.newGame("Ramesh","Suresh")
 
 console.log(g1.play(0));
-console.log(g2.play(0));
 console.log(g1.play(4));
-console.log(g2.play(7));
 console.log(g1.play(1));
-console.log(g2.play(5));
-
+console.log(g1.play(6));
+console.log(g1.play(2));
+//console.log(g1.play(5));
